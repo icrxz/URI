84217fe22b0d737d7222ca08b40ff7e11894575a
Igor Cruz <41965470+icrxz@users.noreply.github.com>
2018-08-05 23:39:37 -0300

    Add files via upload

---
+++ C/1930/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_1930
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] qtd = Console.ReadLine().Split(' ');
+            int tmds = int.Parse(qtd[0]) + int.Parse(qtd[1]) + int.Parse(qtd[2]) + int.Parse(qtd[3]);
+            Console.WriteLine(tmds - 3);
+            Console.ReadKey();
+        }
+    }
+}

7bb4f08f68fe5962956d983fe5bcfbfd0c06e08c
Igor Cruz <41965470+icrxz@users.noreply.github.com>
2018-08-05 23:37:49 -0300

    Add files via upload

---
+++ C/1185/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_1185
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ope = Convert.ToChar(Console.ReadLine());
+            double[,] matx = new double[12, 12];
+            double sum = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    double aux = double.Parse(Console.ReadLine());
+                    matx[i, j] = aux;
+                    if (j < 11-i)
+                        sum += aux;
+                }
+            }
+            if (ope == 'S')
+                Console.WriteLine(sum.ToString("0.0"));
+            else if (ope == 'M')
+                Console.WriteLine((sum / 66).ToString("0.0"));
+            Console.ReadLine();
+        }
+    }
+}

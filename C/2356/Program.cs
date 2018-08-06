c2eaf81df6c2caf2f8a64b4635e0c6f9592560e6
Igor Cruz <41965470+icrxz@users.noreply.github.com>
2018-08-05 23:40:40 -0300

    Add files via upload

---
+++ C/2356/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_2356
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string D;
+            while (!String.IsNullOrEmpty((D = Console.ReadLine())))
+            {
+                string S = Console.ReadLine();
+                if (D.Contains(S))
+                    Console.WriteLine("Resistente");
+                else
+                    Console.WriteLine("Nao resistente");
+            }
+        }
+    }
+}

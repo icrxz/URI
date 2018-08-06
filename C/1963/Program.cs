16707a668940637a93a908209ca774895db128e6
Igor Cruz <41965470+icrxz@users.noreply.github.com>
2018-08-05 23:39:52 -0300

    Add files via upload

---
+++ C/1963/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_1963
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B;
+            string[] input = Console.ReadLine().Split(' ');
+            A = double.Parse(input[0]);
+            B = double.Parse(input[1]);
+            Console.WriteLine((((B / A) * 100) - 100).ToString("0.00") + '%');
+            Console.ReadLine();
+        }
+    }
+}

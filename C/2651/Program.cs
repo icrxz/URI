45e221a3530296c00f88eedf9079c7f18f9bdf8f
Igor Cruz <41965470+icrxz@users.noreply.github.com>
2018-08-05 23:27:46 -0300

    Add files via upload

---
+++ C/2651/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_2651
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = Console.ReadLine();
+            if (txt.ToLower().Contains("zelda"))
+                Console.WriteLine("Link Bolado");
+            else
+                Console.WriteLine("Link Tranquilo");
+            Console.ReadLine();
+        }
+    }
+}
